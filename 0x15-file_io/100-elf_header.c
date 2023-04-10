0f91ea9fb04e589b61d2f14d8f86b17599b14016
Lawrence <larryj645@gmail.com>
2023-04-10 15:47:11 +0000

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,108 +1,100 @@
+#include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <elf.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - displays the information contained in the ELF header at the start
+ * of an ELF file.
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Return: 0 on success, otherwise 98
+ */
+int main(int argc, char **argv)
 {
-    int fd;
-    Elf64_Ehdr header;
+        int fd, ret;
+        Elf64_Ehdr header;
+        unsigned char magic[4] = {0x7f, 0x45, 0x4c, 0x46};
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        exit(98);
-    }
+        if (argc != 2)
+        {
+                dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+                exit(98);
+        }
 
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1) {
-        fprintf(stderr, "Error: cannot open file %s: %s\n", argv[1], strerror(errno));
-        exit(98);
-    }
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Cannot read file %s\n", argv[1]);
+                exit(98);
+        }
 
-    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
-        fprintf(stderr, "Error: cannot read ELF header from file %s: %s\n", argv[1], strerror(errno));
-        exit(98);
-    }
+        ret = read(fd, &header, sizeof(header));
+        if (ret == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Cannot read file %s\n", argv[1]);
+                close(fd);
+                exit(98);
+        }
 
-    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
-        fprintf(stderr, "Error: %s is not an ELF file\n", argv[1]);
-        exit(98);
-    }
+        if (header.e_ident[0] != magic[0] || header.e_ident[1] != magic[1] ||
+            header.e_ident[2] != magic[2] || header.e_ident[3] != magic[3])
+        {
+                dprintf(STDERR_FILENO, "Error: Not an ELF file - it has the wrong magic bytes\n");
+                close(fd);
+                exit(98);
+        }
 
-    printf("ELF Header:\n");
-    printf("  Magic:   ");
-    for (int i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", header.e_ident[i]);
-    }
-    printf("\n");
+        printf("ELF Header:\n");
 
-    printf("  Class:                             ");
-    switch (header.e_ident[EI_CLASS]) {
-        case ELFCLASS32:
-            printf("ELF32\n");
-            break;
-        case ELFCLASS64:
-            printf("ELF64\n");
-            break;
-        default:
-            printf("<unknown>\n");
-            break;
-    }
+        printf("  Magic:   ");
+        for (int i = 0; i < EI_NIDENT; i++)
+                printf("%02x ", header.e_ident[i]);
+        printf("\n");
 
-    printf("  Data:                              ");
-    switch (header.e_ident[EI_DATA]) {
-        case ELFDATA2LSB:
-            printf("2's complement, little endian\n");
-            break;
-        case ELFDATA2MSB:
-            printf("2's complement, big endian\n");
-            break;
-        default:
-            printf("<unknown>\n");
-            break;
-    }
+        printf("  Class:                             ");
+        if (header.e_ident[EI_CLASS] == ELFCLASS32)
+                printf("ELF32\n");
+        else if (header.e_ident[EI_CLASS] == ELFCLASS64)
+                printf("ELF64\n");
+        else
+                printf("<unknown: %x>\n", header.e_ident[EI_CLASS]);
 
-    printf("  Version:                           %d (current)\n", header.e_ident[EI_VERSION]);
+        printf("  Data:                              ");
+        if (header.e_ident[EI_DATA] == ELFDATA2LSB)
+                printf("2's complement, little endian\n");
+        else if (header.e_ident[EI_DATA] == ELFDATA2MSB)
+                printf("2's complement, big endian\n");
+        else
+                printf("<unknown: %x>\n", header.e_ident[EI_DATA]);
 
-    printf("  OS/ABI:                            ");
-    switch (header.e_ident[EI_OSABI]) {
+        printf("  Version:                           %d (current)\n",
+               header.e_ident[EI_VERSION]);
+
+        printf("  OS/ABI:                            ");
+        switch (header.e_ident[EI_OSABI])
+        {
         case ELFOSABI_SYSV:
-            printf("UNIX - System V\n");
-            break;
+                printf("UNIX - System V\n");
+                break;
         case ELFOSABI_LINUX:
-            printf("UNIX - Linux\n");
-            break;
+                printf("UNIX - Linux\n");
+                break;
+        case ELFOSABI_SOLARIS:
+                printf("UNIX - Solaris\n");
+                break;
+        case ELFOSABI_FREEBSD:
+                printf("UNIX - FreeBSD\n");
+                break;
         case ELFOSABI_NETBSD:
-            printf("UNIX - NetBSD\n");
-            break;
-        default:
-            printf("<unknown>\n");
-            break;
-    }
-
-    printf("  ABI Version:                       %d\n", header.e_ident[EI_ABIVERSION]);
-
-    printf("  Type:                              ");
-    switch (header.e_type) {
-        case ET_NONE:
-            printf("NONE (Unknown type)\n");
-            break;
-        case ET_REL:
-            printf("REL (Relocatable file)\n");
-            break;
-        case ET_EXEC:
-            printf("EXEC (Executable file)\n");
-            break;
-        case ET_DYN:
-            printf("DYN (Shared object file)\n");
-            break;
-        case ET_CORE:
-            printf("CORE (Core file)\n");
-            break;
-        default:
-            printf("<unknown>\n");
-            break;
-
+                printf("UNIX - NetBSD\n");
+                break;
+        case ELFOSABI_OPENBSD:
+                printf("UNIX - OpenBSD\n");
+                break;
+        case ELFOSABI_ARM:
+                printf("
