136113e5009681dcf39015374392b14542668645
Lawrence <larryj645@gmail.com>
2023-04-10 15:36:15 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,45 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "holberton.h"
+
+#define BUFFER_SIZE 1024
 
 /**
- * main - Copies the content of a file to another file.
+ * main - Copies the content of one file to another file.
+ * @argc: The number of arguments passed.
+ * @argv: An array of pointers to the arguments.
  *
- * @argc: The number of arguments.
- * @argv: The array of arguments.
- *
- * Return: 0 on success.
+ * Return: 0 on success, or an error code on failure.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, r, w;
-	char buffer[1024];
+	int file_from, file_to, read_result, write_result;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	do {
-		r = read(fd_from, buffer, 1024);
-		if (r == -1)
+		read_result = read(file_from, buffer, BUFFER_SIZE);
+		if (read_result == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-		w = write(fd_to, buffer, r);
-		if (w == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	} while (r == 1024);
 
-	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+		write_result = write(file_to, buffer, read_result);
+		if (write_result == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	} while (read_result == BUFFER_SIZE);
+
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 
 	return (0);
 }
-
